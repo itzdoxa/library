590757e7fa70400aa7d0f9da1ab9b60dc637a44a
itzdoxa <153353785+itzdoxa@users.noreply.github.com>
2024-07-08 16:46:57 +0530

    added ui

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector("#container");
 const addBook = document.querySelector("#addBook");
 const form = document.querySelector("#form");
+const openForm = document.querySelector("#open-form");
+const closeForm = document.querySelector(".close-form")
+const modal = document.querySelector("#modal");
 const bookAuthor = document.querySelector("#author");
 const bookTitle = document.querySelector("#title");
 const bookPages = document.querySelector("#pages");
@@ -15,9 +18,12 @@ function Book(title, author, pages, read) {
 };
 
 const book1 = new Book("one piece", "oda", 1000, "read");
-myLibrary.push(book1);
 const book2 = new Book("naruto", "kishimoto", 800, "read");
-myLibrary.push(book2);
+const book3 = new Book("death note", "Takeshi Obata", 500, "read");
+const book4 = new Book("Bleach", "Tite Kubo", 900, "not-read");
+const book5 = new Book("Berserk", "Kentaro Miura", 1300, "read")
+
+myLibrary.push(book1, book2, book3, book4, book5);
 display();
 
 function createCard(book, index) {
@@ -27,54 +33,61 @@ function createCard(book, index) {
 
         const titleElement = document.createElement("div");
         titleElement.textContent = book.title;
+        titleElement.classList.add("book-title");
         
         const authorElement = document.createElement("div");
         authorElement.textContent = book.author;
+        authorElement.classList.add("book-author");
   
         const pagesElement = document.createElement("div");
         pagesElement.textContent = book.pages;
+
+        const bookInfoContainer = document.createElement("div");
+        bookInfoContainer.classList.add("book-info");
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add("button-container");
   
-        const readElement = document.createElement("div");
+        const readElement = document.createElement("button");
         readElement.textContent = book.read;
         readElement.setAttribute('id', 'readElement');
-
-        changeStatus = document.createElement("button");
-        changeStatus.setAttribute('id', 'changeStatusBtn');
-        changeStatus.textContent = "change reading status";
 
         deleteBook = document.createElement("button");
         deleteBook.textContent = "delete book";
         deleteBook.setAttribute('id', 'deleteBookBtn');
 
-        card.appendChild(titleElement);
-        card.appendChild(authorElement);
-        card.appendChild(pagesElement);
-        card.appendChild(readElement);
-        card.appendChild(changeStatus);
-        card.appendChild(deleteBook);
+        bookInfoContainer.appendChild(titleElement);
+        bookInfoContainer.appendChild(authorElement);
+        bookInfoContainer.appendChild(pagesElement);
+        
+        buttonContainer.appendChild(readElement);
+        buttonContainer.appendChild(deleteBook);
+        card.appendChild(bookInfoContainer);
+        card.appendChild(buttonContainer);
 
         return card;
-  };   
+  }; 
 
-container.addEventListener('click', function(event){
-      if(event.target.tagName === "BUTTON") {
-                 const card = event.target.closest(".card");
-                 const index = card.getAttribute('data-bookNumber');
-    
-              if(event.target.getAttribute('id') === "deleteBookBtn"){
-                 container.removeChild(card);
-                 myLibrary.splice(index, 1);
-                 updateBookNumbers();
-                 console.log("number of books in library after deletion: " + myLibrary.length)
-              } 
+function display(){
+    myLibrary.forEach((book, index)=> {
+        if(!container.querySelector(`[data-bookNumber="${index}"]`)) {
+                const card = createCard(book, index);
+                container.appendChild(card);
+                const readElement = card.querySelector("#readElement");
+                colorReadElement(index, readElement);
+          };     
+    });
+};
 
-              else if(event.target.getAttribute('id') === "changeStatusBtn"){
-                myLibrary[index].read = myLibrary[index].read === 'read' ? 'not read' : 'read';
-                card.querySelector("#readElement").textContent = myLibrary[index].read;
-              }
-
-      };         
-});
+function colorReadElement(index, readElement){
+        if(myLibrary[index].read === 'read') {
+            readElement.classList.add('read')
+            readElement.classList.remove('not-read')
+        } else if(myLibrary[index].read === 'not-read') {
+            readElement.classList.add('not-read')
+            readElement.classList.remove('read')
+        }
+};
 
 function updateBookNumbers() {
   const cards = container.querySelectorAll('.card');
@@ -83,18 +96,29 @@ function updateBookNumbers() {
   });
 };
 
-function display(){
-    myLibrary.forEach((book, index)=> {
-        if(!container.querySelector(`[data-bookNumber="${index}"]`)) {
-                const card = createCard(book, index);
-                container.appendChild(card);
-          }     
-    });
-};
+container.addEventListener('click', function(event){
+  if(event.target.tagName === "BUTTON") {
+             const card = event.target.closest(".card");
+             const index = card.getAttribute('data-bookNumber');
+             const readElement = card.querySelector("#readElement");
+
+          if(event.target.getAttribute('id') === "deleteBookBtn"){
+             container.removeChild(card);
+             myLibrary.splice(index, 1);
+             updateBookNumbers();
+             console.log("number of books in library after deletion: " + myLibrary.length)
+          } 
+
+          else if(event.target.getAttribute('id') === "readElement"){
+            myLibrary[index].read = myLibrary[index].read === 'read' ? 'not-read' : 'read';
+            readElement.textContent = myLibrary[index].read
+            colorReadElement(index, readElement);
+          }
+
+  };         
+});
 
 form.addEventListener('submit', function(event){
-     event.preventDefault()
-
      const bookExists = (currentbook) => {
             return currentbook.title === bookTitle.value && currentbook.author === bookAuthor.value;
         };
@@ -109,4 +133,16 @@ form.addEventListener('submit', function(event){
         myLibrary.push(userBook);
         display();
       }
+      modal.close();
+      form.reset();
 });
+
+openForm.addEventListener('click', ()=> {
+  modal.showModal();
+});
+
+closeForm.addEventListener('click', () => {
+  modal.close();
+})
+
+
